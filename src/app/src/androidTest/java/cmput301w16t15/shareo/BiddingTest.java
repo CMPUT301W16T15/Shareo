8ebc43e7eaa3f934d73c19787f7bf38bc1b69cae
Alex <alex.sampley@gmail.com>
2016-02-09 16:54:13 -0700

    Modified test file for bidding on a Thing

--- src/app/src/androidTest/java/cmput301w16t15/shareo/BiddingTest.java
+++ src/app/src/androidTest/java/cmput301w16t15/shareo/BiddingTest.java
@@ -11,28 +11,32 @@ public class BiddingTest extends ActivityInstrumentationTestCase2 {
     }
 
     public void testBidOnThing() {
+        User bidder = new User("Frank");
         Thing t1 = new Thing(Thing.AVAILABLE);
         Thing t2 = new Thing(Thing.BIDDED);
         Thing t3 = new Thing(Thing.BORROWED);
 
         try {
-            t1.placeBid(0.1);
+            t1.placeBid(bidder, 0.1);
             assertTrue(t1.getStatus() == Thing.BIDDED);
+            assertTrue(t1.getBidders().contains(bidder));
         } catch (ThingUnavailableException e) {
             fail();
         }
         try {
-            t2.placeBid(0.2);
+            t2.placeBid(bidder, 0.2);
             assertTrue(t2.getStatus() == Thing.BIDDED);
+            assertTrue(t1.getBidders().contains(bidder));
         } catch (ThingUnavailableException e) {
             fail();
         }
 
         try {
-            t1.placeBid(0.1);
+            t1.placeBid(bidder, 0.1);
             fail();
         } catch (ThingUnavailableException e) {
             assertTrue(t1.getStatus() == Thing.BORROWED);
+            assertFalse(t1.getBidders().contains(bidder));
         }
     }
 }
