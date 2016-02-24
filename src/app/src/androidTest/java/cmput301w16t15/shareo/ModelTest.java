b39084496f19f84e707fd5d8cd5526a0038d7fec
Alex <alex.sampley@gmail.com>
2016-02-24 13:40:12 -0700

    Added tests to ModelTest for interactions between User and Thing when borrowing

--- src/app/src/androidTest/java/cmput301w16t15/shareo/ModelTest.java
+++ src/app/src/androidTest/java/cmput301w16t15/shareo/ModelTest.java
@@ -14,7 +14,7 @@ public class ModelTest extends ActivityInstrumentationTestCase2 {
         super(MainActivity.class);
     }
 
-    public void testAddOwner() {
+    public void testSetOwner() {
         User u1 = new User("Bob");
         User u2 = new User("Susan");
         Thing t1 = new Game("A card game");
@@ -37,6 +37,30 @@ public class ModelTest extends ActivityInstrumentationTestCase2 {
         assertTrue(t1.getOwner() == null);
         assertTrue(!u1.getOwnedThings().contains(t1));
         assertTrue(!u2.getOwnedThings().contains(t1));
+    }
 
+    public void testSetBorrower() {
+        User u1 = new User("Bob");
+        User u2 = new User("Susan");
+        Thing t1 = new Game("A card game");
+
+        assertTrue(t1.getBorrower() == null);
+        assertTrue(!u1.getBorrowedThings().contains(t1));
+        assertTrue(!u2.getBorrowedThings().contains(t1));
+
+        t1.setBorrower(u1);
+        assertTrue(t1.getBorrower().equals(u1));
+        assertTrue(u1.getBorrowedThings().contains(t1));
+        assertTrue(!u2.getBorrowedThings().contains(t1));
+
+        t1.setBorrower(u2);
+        assertTrue(t1.getBorrower().equals(u2));
+        assertTrue(!u1.getBorrowedThings().contains(t1));
+        assertTrue(u2.getBorrowedThings().contains(t1));
+
+        t1.setBorrower(null);
+        assertTrue(t1.getBorrower() == null);
+        assertTrue(!u1.getBorrowedThings().contains(t1));
+        assertTrue(!u2.getBorrowedThings().contains(t1));
     }
 }
