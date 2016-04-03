b0669fd977e3017c26d18af6b34eb817f785e3fa
Alex <alex.sampley@gmail.com>
2016-04-02 18:06:10 -0600

    Fixed some formatting of dollars in some views

--- app/src/main/java/cmput301w16t15/shareo/AddBidFragment.java
+++ app/src/main/java/cmput301w16t15/shareo/AddBidFragment.java
@@ -222,7 +222,7 @@ public class AddBidFragment extends DialogFragment {
         mtextViewDescription.setText(mtextViewDescription.getText() + ": " + mThing.getDescription());
         mtextViewNumberPlayers.setText(mtextViewNumberPlayers.getText() + ": " + mThing.getNumberPlayers());
         mtextViewCategory.setText(mtextViewCategory.getText() + ": " + mThing.getCategory());
-        mtextViewCurrentTopBid.setText(mtextViewCurrentTopBid.getText() + ": $" + String.valueOf((double) mThing.getTopBidAmount() / 100));
+        mtextViewCurrentTopBid.setText(String.format("%s: $%d.%02d", mtextViewCurrentTopBid.getText(), (mThing.getTopBidAmount() / 100), (mThing.getTopBidAmount() % 100)));
         if (mThing.getPhotoModel() != null) {
             mimageViewGame.setImageBitmap(mThing.getPhotoModel().getPhoto());
         }
